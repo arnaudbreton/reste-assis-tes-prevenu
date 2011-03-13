b6394d017918082c870a0f24754ed8cd63c91f3e
Arnaud BRETON <arn.breton@gmail.com>
2011-03-13 19:51:59 +0000

    Chargement des incidents via AsyncTask

--- ResteAssisTesPrevenu/src/com/android/resteassistesprevenu/activities/IncidentsEnCours.java
+++ ResteAssisTesPrevenu/src/com/android/resteassistesprevenu/activities/IncidentsEnCours.java
@@ -13,53 +13,88 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 
 import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.resteassistesprevenu.R;
 import com.android.resteassistesprevenu.model.IncidentModel;
 import com.android.resteassistesprevenu.model.adapters.IncidentModelArrayAdapter;
 
-public class IncidentsEnCours extends ListActivity {
-
-	String serviceURLBase = "http://openreact.alwaysdata.net";
-	String serviceURLByHour = "/api/incidents.json/hour";
-	String serviceURLRunning = "/api/incidents.json/current";
+public class IncidentsEnCours extends ListActivity {	
+	
+	private ProgressDialog pd; 
+	private ArrayList<IncidentModel> incidents;
+	private LoadIncidentsAsyncTask loadIncidentsTask;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// setContentView()
 		
-		ArrayList<IncidentModel> incidents = new ArrayList<IncidentModel>();
-		try {
-			incidents = IncidentModel.deserializeFromArray(getIncidentsEnCours());
-		} catch (JSONException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		setListAdapter(new IncidentModelArrayAdapter(this, android.R.layout.simple_list_item_1, incidents));
+		new LoadIncidentsAsyncTask(this).execute();
 	}
+	
+	private class LoadIncidentsAsyncTask extends AsyncTask<Void, Void, Void> 
+	{
+		private String serviceURLBase = "http://openreact.alwaysdata.net";
+		private String serviceURLByHour = "/api/incidents.json/hour";
+		private String serviceURLRunning = "/api/incidents.json/current";
 
-	private String getIncidentsEnCours() throws JSONException {
-		HttpClient httpclient = new DefaultHttpClient();
-		HttpGet request = new HttpGet(this.serviceURLBase + this.serviceURLRunning);
-		String result = null; 
+		private Context c; 
+		private ProgressDialog pd;
 		
-		//request.addHeader("deviceId", deviceId);
-		ResponseHandler<String> handler = new BasicResponseHandler();
-		try {
-			result = httpclient.execute(request, handler);
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		public LoadIncidentsAsyncTask(Context c) {
+			this.c = c;
 		}
-		httpclient.getConnectionManager().shutdown();
-		Log.i("ResteAssisTesPrevenu : ", result);
 		
-		return result;
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pd = ProgressDialog.show(c, "", "Chargement des incidents en cours");
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			pd.dismiss();
+			
+		    setListAdapter(new IncidentModelArrayAdapter(  
+                    IncidentsEnCours.this, R.layout.incident_item_view, incidents));  
+		}		
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				incidents = IncidentModel.deserializeFromArray(getIncidentsEnCours());
+			} catch (JSONException e) {
+				return null;
+			} catch (ParseException e) {
+				return null;
+			}			
+			
+			return null;
+		}
+		
+		private String getIncidentsEnCours() throws JSONException {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet request = new HttpGet(this.serviceURLBase + this.serviceURLRunning);
+			String result = null; 		
+			
+			ResponseHandler<String> handler = new BasicResponseHandler();
+			try {
+				result = httpclient.execute(request, handler);
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			httpclient.getConnectionManager().shutdown();
+			Log.i("ResteAssisTesPrevenu : ", result);
+			
+			return result;
+		}	
 	}
 }
