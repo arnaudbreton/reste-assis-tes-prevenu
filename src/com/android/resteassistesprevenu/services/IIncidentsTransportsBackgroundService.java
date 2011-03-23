9d0d8ec1e23f864a67c112dd961bfa29d3166e76
Arnaud BRETON <arn.breton@gmail.com>
2011-03-23 14:40:38 +0000

    implémentation type_lignes ContentProvider

--- src/com/android/resteassistesprevenu/services/IIncidentsTransportsBackgroundService.java
+++ src/com/android/resteassistesprevenu/services/IIncidentsTransportsBackgroundService.java
@@ -2,13 +2,14 @@ package com.android.resteassistesprevenu.services;
 
 import com.android.resteassistesprevenu.services.listeners.IIncidentsTransportsBackgroundServiceGetIncidentsEnCoursListener;
 import com.android.resteassistesprevenu.services.listeners.IIncidentsTransportsBackgroundServiceGetLignesListener;
+import com.android.resteassistesprevenu.services.listeners.IIncidentsTransportsBackgroundServiceGetTypeLignesListener;
 
 public interface IIncidentsTransportsBackgroundService {	
 	public void addGetIncidentsListener(IIncidentsTransportsBackgroundServiceGetIncidentsEnCoursListener listener);
 	public void removeGetIncidentsListener(IIncidentsTransportsBackgroundServiceGetIncidentsEnCoursListener listener);
 	
-	public void addGetLignesListener(IIncidentsTransportsBackgroundServiceGetLignesListener listener);
-	public void removeGetLignesListener(IIncidentsTransportsBackgroundServiceGetLignesListener listener);
+	public void addGetTypeLignesListener(IIncidentsTransportsBackgroundServiceGetTypeLignesListener listener);
+	public void removeGetTypeLignesListener(IIncidentsTransportsBackgroundServiceGetTypeLignesListener listener);
 
 	/**
 	 * Recherche des incidents, asynchrone
