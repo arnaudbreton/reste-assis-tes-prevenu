7cb2e9c4da6e71ddecc50d2996a8ea5eb0634340
Arnaud BRETON <arn.breton@gmail.com>
2011-03-28 19:46:50 +0000

    Vue Favoris

--- src/com/resteassistesprevenu/activities/listeners/IIncidentActionListener.java
+++ src/com/resteassistesprevenu/activities/listeners/IIncidentActionListener.java
@@ -1,7 +1,8 @@
 package com.resteassistesprevenu.activities.listeners;
 
 import com.resteassistesprevenu.model.IncidentAction;
+import com.resteassistesprevenu.model.IncidentModel;
 
 public interface IIncidentActionListener {
-	public void actionPerformed(int incidentId, IncidentAction action);
+	public void actionPerformed(IncidentModel incident, IncidentAction action);
 }
--- src/com/resteassistesprevenu/model/LigneModel.java
+++ src/com/resteassistesprevenu/model/LigneModel.java
@@ -1,18 +1,11 @@
 package com.resteassistesprevenu.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class LigneModel {
 	private int id;
-	private TypeLigne mTypeLigne;
+	private String mTypeLigne;
 	private String mNumLigne;
 	
-	public LigneModel(int id, TypeLigne typeLigne, String numLigne) {
+	public LigneModel(int id, String typeLigne, String numLigne) {
 		this.id = id;
 		this.mTypeLigne = typeLigne;
 		this.mNumLigne = numLigne;
@@ -28,7 +21,7 @@ public class LigneModel {
 	/**
 	 * @return the mTypeLigne
 	 */
-	public TypeLigne getTypeLigne() {
+	public String getTypeLigne() {
 		return mTypeLigne;
 	}
 
@@ -39,37 +32,8 @@ public class LigneModel {
 		return mNumLigne;
 	}
 	
-	public static List<LigneModel> deserializeArray(String serializedLignesArray) throws JSONException {
-		JSONArray jsonArray = new JSONArray(serializedLignesArray);
-		
-		List<LigneModel> lignes = new ArrayList<LigneModel>();
-		for(int i=0;i<jsonArray.length();i++) {
-			JSONObject ligne;
-			ligne = jsonArray.getJSONObject(i);
-			lignes.add(deserializeFromJSONObj(ligne));
-		}
-		
-		return lignes;
-	}
-
-	private static LigneModel deserializeFromJSONObj(JSONObject ligne) throws JSONException {
-		int ligneId = ligne.getInt("uid");
-		
-		String strTypeLigne = ligne.getString("name").split(" ")[0];
-		TypeLigne typeLigne;
-		
-		String numLigne = ligne.getString("name").split(" ")[1];
-		
-		if(strTypeLigne.equals("Métro")) {
-			typeLigne = TypeLigne.METRO;
-		}		
-		else if(strTypeLigne.equals("RER")) {
-			typeLigne = TypeLigne.RER;
-		}
-		else {
-			typeLigne = TypeLigne.TRANSILIEN;
-		}			
-		
-		return new LigneModel(ligneId, typeLigne, numLigne);
+	@Override
+	public String toString() {
+		return getTypeLigne() + " " + getNumLigne();
 	}
 }
