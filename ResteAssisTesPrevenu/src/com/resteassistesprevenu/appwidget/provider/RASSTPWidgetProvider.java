be60847a288dbf8b4b5d66310fef4a5a1692c1e5
Arnaud BRETON <arn.breton@gmail.com>
2011-04-24 18:23:32 +0000

    Création du service du Widget

--- ResteAssisTesPrevenu/src/com/resteassistesprevenu/appwidget/provider/RASSTPWidgetProvider.java
+++ ResteAssisTesPrevenu/src/com/resteassistesprevenu/appwidget/provider/RASSTPWidgetProvider.java
@@ -1,36 +1,33 @@
 package com.resteassistesprevenu.appwidget.provider;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.RemoteViews;
 
-import com.resteassistesprevenu.R;
-import com.resteassistesprevenu.activities.IncidentsEnCoursActivity;
-import com.resteassistesprevenu.activities.NewIncidentActivity;
+import com.resteassistesprevenu.appwidget.service.UpdateService;
 
 public class RASSTPWidgetProvider extends AppWidgetProvider {
+	/**
+	 * Intent pour le service de mise à jour
+	 */
+	private Intent intentService;
 
 	@Override
-	public void onUpdate(Context context, android.appwidget.AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		final int N = appWidgetIds.length;
-
-        // Perform this loop procedure for each App Widget that belongs to this provider
-        for (int i=0; i<N; i++) {
-        	int appWidgetId = appWidgetIds[i];
-        	 
-        	RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.rasstp_appwidget);
-			
-        	Intent incidentEnCoursIntent = new Intent(context, IncidentsEnCoursActivity.class);
-	        PendingIntent incidentEnCoursPendingIntent = PendingIntent.getActivity(context, 0, incidentEnCoursIntent, 0);
-	        views.setOnClickPendingIntent(R.id.btnWidgetLogo, incidentEnCoursPendingIntent);
-	        
-	     	Intent newIncidentIntent = new Intent(context, NewIncidentActivity.class);
-	        PendingIntent newIncidentPendingIntent = PendingIntent.getActivity(context, 0, newIncidentIntent, 0);
-	        views.setOnClickPendingIntent(R.id.btnWidgetAddIncident, newIncidentPendingIntent);
-	        
-	        appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
+	public void onEnabled(Context context) {
+		// TODO Auto-generated method stub
+		super.onEnabled(context);
+		
+		this.intentService = new Intent(context, UpdateService.class);
+		context.startService(intentService);
 	}
+	@Override
+	public void onUpdate(Context context,
+			android.appwidget.AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+	}
+	
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {		
+		super.onDeleted(context, appWidgetIds);
+	}	
 }
