72af3dd9565b646b35309fb7a41d499c6d7b8f92
Arnaud BRETON <arn.breton@gmail.com>
2011-05-10 23:05:20 +0000

    Nettoyage import.

--- src/com/resteassistesprevenu/activities/IncidentsEnCoursActivity.java
+++ src/com/resteassistesprevenu/activities/IncidentsEnCoursActivity.java
@@ -13,7 +13,6 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.Menu;
